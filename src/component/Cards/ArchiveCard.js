88a5e9d01f74be201a3703824dab53c42cff2597
Divya70 <divyanamdev8795@gmail.com>
2022-07-18 20:39:55 +0530

    Feat: add functionality

--- src/component/Cards/ArchiveCard.js
+++ src/component/Cards/ArchiveCard.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "../Cards/css/habitcard.css";
-const ArchiveCard = () => {
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addhabit, addarchive } from "../../redux/reducers/HabitSlice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const ArchiveCard = (props) => {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem("token");
+  const dltHabit = async () => {
+    const res = await axios.delete(`/api/archives/${props.data._id}`, {
+      headers: {
+        authorization: token,
+      },
+    });
+    if (res.status === 200) {
+      dispatch(addarchive(res.data.archives));
+    } else {
+      alert("error");
+    }
+  };
+  const notify = () => toast("Some Error Occured, refresh and retry");
+  const restoreHabit = async () => {
+    const res = await axios.post(
+      `/api/archives/restore/${props.data._id}`,
+      {},
+      {
+        headers: {
+          authorization: token,
+        },
+      }
+    );
+    if (res.status === 200) {
+      dispatch(addarchive(res.data.archives));
+      dispatch(addhabit(res.data.habits));
+    } else {
+      notify();
+    }
+  };
   return (
     <>
+      <ToastContainer />
       <div className="habit-card-sec">
         <div className="card-profile">
           <img
@@ -11,12 +49,12 @@ const ArchiveCard = () => {
           />
         </div>
         <div className="card-user-name">
-          <span>Name</span>
-          <span>Goal</span>
+          <span>{props.data.name}</span>
+          <span>{props.data.reminder}</span>
         </div>
         <div className="card-btn-sec">
-          <button>Delete</button>
-          <button>Restore</button>
+          <button onClick={dltHabit}>Delete</button>
+          <button onClick={restoreHabit}>Restore</button>
         </div>
       </div>
     </>
